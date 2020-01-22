69ea8c83ce5bfb430a776f1067fe97a24523f8ed
Skyus <ekaskyus@gmail.com>
2020-01-22 14:27:54 +0200

    Initial Commit

---
+++ TicTacToeGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TicTacToeGame
+//
+//  Created by auc on 1/22/20.
+//  Copyright © 2020 AUC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
